573a1cb1eefaf3c8a5d19efd83570c1a5fc85aba
dsmathers <dsmathers91@gmail.com>
2021-08-09 10:31:05 -0700

    Set up React App for Portfolio Website overhaul

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav id="navbar">
+            <a href="#top" id="logo">&lt;DS /&gt;</a>
+        <ul id="page_nav_list">
+            <a href="#about" className="page_nav">About</a>
+            <a href="#projects" className="page_nav">Projects</a> 
+            <a href="#contact" className="page_nav">Contact</a> 
+            </ul>
+        </nav>
+      
+    )
+}
+
+export default Navbar
