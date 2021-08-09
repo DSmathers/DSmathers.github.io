cb8cfa6ae5b38fe580118e9ccfa494ceeb3027b4
dsmathers <dsmathers91@gmail.com>
2021-08-09 14:41:06 -0700

    Added Welcome Banner Componenet plus CSS

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+const Banner = () => {
+    return (
+        <section id="welcome-section" className="page_section">
+            <div id="welcome-tag">
+                <h1>Hello, I'm Daniel Smathers</h1>
+                <h2>A Web Developer located in Portland, Oregon</h2>
+            </div>
+        </section>
+    )
+}
+
+export default Banner
