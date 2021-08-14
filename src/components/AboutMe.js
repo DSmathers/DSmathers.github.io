5739d74869a677314d8744389b029641a441c3f4
dsmathers <dsmathers91@gmail.com>
2021-08-13 19:08:34 -0700

    removed duplicate project data, added blackjack.py and technical documentation to project data, fixed a couple links

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,14 +11,14 @@ const AboutMe = () => {
                         My story begins with years of tinkering around with Microsoft Excel and Google Sheets, building "applications" for everything
                         from my personal budget, inventory management in a retail business I ran, data entry, a small scale CRM, and so on.
                         It wasn't until January 2021 that I came accross 
-                        <a href="#" target="_blank" rel="noreferrer"> Introduction to Computer Science and Programming</a> offered by
-                        <a href="#" target="_blank" rel="noreferrer"> MITOPENCOURSEWARE</a>, which opened my eyes to the world of programming, ultimately leading me to embark on the journey
+                        <a href="https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-00sc-introduction-to-computer-science-and-programming-spring-2011/index.htm" target="_blank" rel="noreferrer"> Introduction to Computer Science and Programming</a> offered by
+                        <a href="https://ocw.mit.edu/" target="_blank" rel="noreferrer"> MITOPENCOURSEWARE</a>, which opened my eyes to the world of programming, ultimately leading me to embark on the journey
                         of becoming a developer.
                     </p>
                     <p className="about_me_text">
                         After completion of that course I went on to complete the <a href="https://www.freecodecamp.org/certification/smathers_daniel/responsive-web-design" target="_blank" rel="noreferrer">
                         Responsive Web Design</a> and <a href="https://www.freecodecamp.org/certification/smathers_daniel/javascript-algorithms-and-data-structures" target="_blank" rel="noreferrer">
-                        JavaScript Algorithms and Data Structures</a> courses offered by <a href="#" target="_blank" rel="noreferrer">freeCodeCamp;</a> as well as two short courses on Udemy on ES6 and React.
+                        JavaScript Algorithms and Data Structures</a> courses offered by <a href="https://www.freecodecamp.com" target="_blank" rel="noreferrer">freeCodeCamp;</a> as well as two short courses on Udemy on ES6 and React.
                         While I definitely learned a lot, I decided that it would make more sense to learn "on the job" and have since started working towards building applications from the ground up, learning
                         new theories and libraries as I need them instead of all at once.
                     </p>
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,7 @@
 import projectThumb1 from '../assets/thumbnails/projectThumb1.png'
 import projectThumb2 from '../assets/thumbnails/projectThumb2.png'
+import thumb3 from '../assets/thumbnails/thumb3.png'
+import projectThumb4 from '../assets/thumbnails/projectThumb4.png'
 
 const Data =  [
     {
@@ -16,39 +18,23 @@ const Data =  [
         title: "React Portfolio Website",
         description: "I wanted to practice using React, where better to start than my personal website?",
         gitLink: "https://github.com/DSmathers/react-portfolio",
-        demoLink: "#",
+        demoLink: "https://dsmathers.github.io",
     },
     {
         key: 3,
-        img: projectThumb2,
-        title: "React Portfolio Website",
-        description: "I wanted to practice using React, where better to start than my personal website?",
-        gitLink: "https://github.com/DSmathers/react-portfolio",
-        demoLink: "#",
+        img: thumb3,
+        title: "Technical Documentation Page",
+        description: "A template that can be used for Technical Documentation. Written in HTML and CSS only.",
+        gitLink: "https://github.com/DSmathers/Technical-Documentation-Skeleton",
+        demoLink: "https://codepen.io/D-Smathers/full/PoGwOwg",
     },
     {
         key: 4,
-        img: projectThumb2,
-        title: "React Portfolio Website",
-        description: "I wanted to practice using React, where better to start than my personal website?",
-        gitLink: "https://github.com/DSmathers/react-portfolio",
-        demoLink: "#",
-    },
-    {
-        key: 5,
-        img: projectThumb2,
-        title: "React Portfolio Website",
-        description: "I wanted to practice using React, where better to start than my personal website?",
-        gitLink: "https://github.com/DSmathers/react-portfolio",
-        demoLink: "#",
-    },
-    {
-        key: 6,
-        img: projectThumb2,
-        title: "React Portfolio Website",
-        description: "I wanted to practice using React, where better to start than my personal website?",
-        gitLink: "https://github.com/DSmathers/react-portfolio",
-        demoLink: "#",
+        img: projectThumb4,
+        title: "Command Line Blackjack",
+        description: "This was one of my first proejcts. Simple game of Blackjack against the computer. Written in Python",
+        gitLink: "https://github.com/DSmathers/Blackjack.py",
+        demoLink: "https://replit.com/@DanielSmathers/Blackjackpy#Blackjack.py",
     },
 ]
 
