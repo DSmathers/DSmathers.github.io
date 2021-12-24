23d07a0cea8cf9dff8afda7b265a7c3dcec2fb32
dsmathers <dsmathers91@gmail.com>
2021-12-23 23:27:54 -0800

    added library project and removed an unused file

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,40 +2,52 @@ import projectThumb1 from '../assets/thumbnails/projectThumb1.png'
 import projectThumb2 from '../assets/thumbnails/projectThumb2.png'
 import thumb3 from '../assets/thumbnails/thumb3.png'
 import projectThumb4 from '../assets/thumbnails/projectThumb4.png'
+import projectThumb5 from '../assets/thumbnails/projectThumb5.png'
 
 const Data =  [
+    {
+        key: 5,
+        img: projectThumb5,
+        title: "Personal Library Manager",
+        description: "Tech Used: HTML, CSS, React.js, localStorage API",
+        gitLink: 'https://github.com/DSmathers/tOP_Library',
+        demoLink: 'https://dsmathers.github.io/tOP_Library/',
+    },
     {
         key: 1,
         img: projectThumb1,
         title: "Compound Interest Calc",
-        description: "Compound Interest Calculator using vanilla JS. I did this to get some practice using user variables",
+        description: "Tech Used: HTML, CSS, JavaScript",
         gitLink: "https://github.com/DSmathers/CompoundInterestCalc",
         demoLink: "https://dsmathers.github.io/CompoundInterestCalc/",
     },
+    
     {
-        key: 2,
-        img: projectThumb2,
-        title: "React Portfolio Website",
-        description: "I wanted to practice using React, where better to start than my personal website?",
-        gitLink: "https://github.com/DSmathers/react-portfolio",
-        demoLink: "https://dsmathers.github.io",
+        key: 4,
+        img: projectThumb4,
+        title: "Command Line Blackjack",
+        description: "Tech Used: Python",
+        gitLink: "https://github.com/DSmathers/Blackjack.py",
+        demoLink: "https://replit.com/@DanielSmathers/Blackjackpy#Blackjack.py",
     },
     {
         key: 3,
         img: thumb3,
         title: "Technical Documentation Page",
-        description: "A template that can be used for Technical Documentation. Written in HTML and CSS only.",
+        description: "Tech Used: HTML and CSS",
         gitLink: "https://github.com/DSmathers/Technical-Documentation-Skeleton",
         demoLink: "https://codepen.io/D-Smathers/full/PoGwOwg",
     },
     {
-        key: 4,
-        img: projectThumb4,
-        title: "Command Line Blackjack",
-        description: "This was one of my first proejcts. Simple game of Blackjack against the computer. Written in Python",
-        gitLink: "https://github.com/DSmathers/Blackjack.py",
-        demoLink: "https://replit.com/@DanielSmathers/Blackjackpy#Blackjack.py",
+        key: 2,
+        img: projectThumb2,
+        title: "React Portfolio Website",
+        description: "Tech Used: React.js and CSS",
+        gitLink: "https://github.com/DSmathers/react-portfolio",
+        demoLink: "https://dsmathers.github.io",
     },
+    
+    
 ]
 
 export default Data;
